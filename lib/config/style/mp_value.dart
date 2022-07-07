9aeb7f917fcce7c451da2c7a2da5e34db922c62b
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-07 14:11:44 +0545

    margin & padding value

---
+++ lib/config/style/mp_value.dart
@@ -0,0 +1,23 @@
+class AppMargin {
+  static const double m5 = 5.0;
+  static const double m8 = 8.0;
+  static const double m10 = 10.0;
+  static const double m12 = 12.0;
+  static const double m14 = 14.0;
+  static const double m15 = 15.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+}
+
+class AppPadding {
+  static const double p5 = 5.0;
+  static const double p8 = 8.0;
+  static const double p10 = 10.0;
+  static const double p12 = 12.0;
+  static const double p14 = 14.0;
+  static const double p15 = 15.0;
+  static const double p16 = 16.0;
+  static const double p18 = 18.0;
+  static const double p20 = 20.0;
+}
