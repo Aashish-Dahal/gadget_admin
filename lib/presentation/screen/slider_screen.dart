aae1bfdeb8ff23f3e22a56e827542294d82b4ea4
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-07 18:02:46 +0545

    fix: slider screen

--- lib/presentation/screen/slider_screen.dart
+++ lib/presentation/screen/slider_screen.dart
@@ -1,9 +1,41 @@
 import 'dart:io';
-import 'package:admin_panel/data/firebase/firebase_base.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
+import 'dart:ui' show ImageFilter;
+import 'package:admin_panel/business/services/firebase_services.dart';
+import 'package:admin_panel/config/style/mp_value.dart';
+import 'package:admin_panel/data/model/slider_model.dart';
+import 'package:flutter/material.dart'
+    show
+        Alignment,
+        AppBar,
+        BackdropFilter,
+        BorderRadius,
+        BoxFit,
+        BuildContext,
+        ClipRRect,
+        Colors,
+        Column,
+        Container,
+        EdgeInsets,
+        ElevatedButton,
+        Image,
+        Key,
+        MainAxisSize,
+        Padding,
+        Scaffold,
+        SizedBox,
+        Stack,
+        StackFit,
+        State,
+        StatefulWidget,
+        Text,
+        TextButton,
+        Theme,
+        TileMode,
+        Widget;
 import 'package:image_picker/image_picker.dart'
     show ImagePicker, ImageSource, XFile;
+
+import '../widgets/button.dart';
 
 class HomeSlider extends StatefulWidget {
   const HomeSlider({Key? key}) : super(key: key);
@@ -19,51 +51,65 @@ class _HomeSliderState extends State<HomeSlider> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Add Slider"),
+        title: const Text("Add Slider Image"),
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: 190,
-                decoration: BoxDecoration(
-                    image: _image != null
-                        ? DecorationImage(
-                            image: FileImage(_image!), fit: BoxFit.cover)
-                        : const DecorationImage(
-                            image: AssetImage(
-                                "assets/images/slider_background.jpg"))),
+      body: Padding(
+        padding: const EdgeInsets.only(
+            top: AppPadding.p10, right: AppPadding.p10, left: AppPadding.p10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 200,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  _image != null
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(_image!, fit: BoxFit.cover))
+                      : Image.asset('assets/images/slider_background.jpg',
+                          fit: BoxFit.cover),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(
+                          sigmaX: 3, sigmaY: 3, tileMode: TileMode.mirror),
+                      child: Container(
+                          color: Colors.black.withOpacity(.2),
+                          alignment: Alignment.center,
+                          child: TextButton(
+                              onPressed: () {
+                                _openImagePicker();
+                              },
+                              child: Text("Select Image",
+                                  style:
+                                      Theme.of(context).textTheme.headline1))),
+                    ),
+                  ),
+                ],
               ),
-              _image != null
-                  ? const SizedBox()
-                  : Align(
-                      alignment: Alignment.topCenter,
-                      child: TextButton(
-                          onPressed: () {
-                            _openImagePicker();
-                          },
-                          child: const Text(
-                            "Select Image",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ))),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-              onPressed: (_image == null)
-                  ? null
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Button(
+              width: double.infinity,
+              size: ButtonSize.large,
+              type: ButtonType.filled,
+              onPressed: _image == null
+                  ? () {}
                   : () async {
-                      TaskState state = await _uploadImage();
-                      if (state == TaskState.success) {
-                        print("success");
+                      String? url = await FireBaseServices.uploadImage(_image!);
+                      if (url != null) {
+                        FireBaseServices.uploadBannerData(
+                            SliderModel(imagePath: url));
                       }
                     },
-              child: const Text("Upload Image"))
-        ],
+              child: const Text("Upload Image"),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -76,17 +122,5 @@ class _HomeSliderState extends State<HomeSlider> {
         _image = File(pickedImage.path);
       });
     }
-  }
-
-  Future<TaskState> _uploadImage() async {
-    final ref = FirebaseBase.storageInstance
-        .ref()
-        .child("sliderImages")
-        .child(_image!.path.split("/cache/").last);
-    var result = await ref.putFile(File(_image!.path));
-    var url = await ref.getDownloadURL();
-    final docUser = FirebaseBase.firestoreInstance.collection('slider').doc();
-    await docUser.set({'id': docUser.id, 'imagePath': url});
-    return result.state;
   }
 }
